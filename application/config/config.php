3f1502ed01427e73b857b4d8a3bfb55bb5df1b79
Arifin <qccikande1.official@gmail.com>
2026-02-25 11:16:40 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://10.7.5.13/ns-form/';
+$config['base_url'] = 'http://localhost/ns-form/';
 
 /*
 |--------------------------------------------------------------------------
